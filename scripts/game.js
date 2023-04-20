94e4ccc05342a2166ab89f0e7da5f0225ebda2cb
FelippeRibeiro <felipper43@gmail.com>
2023-04-20 19:55:51 -0300

    Add: Stateatment end game

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,5 @@
+const initState = document;
+console.log(initState);
 const jeff = document.getElementById("jeff");
 const pipe = document.getElementById("pipe");
 const gameboard = document.getElementById("game-board");
@@ -7,7 +9,6 @@ const assets = [
 	"./styles/images/pipe.png",
 	"./styles/images/penis.png",
 	"./styles/images/bullet.png",
-	"",
 ];
 
 function jump() {
@@ -26,5 +27,32 @@ document.addEventListener("keydown", (key) => {
 document.addEventListener("click", jump);
 
 setInterval(() => {
-	pipe.src = assets[Math.floor(Math.random() * 4)];
+	pipe.src = assets[Math.floor(Math.random() * 3)];
 }, 2400);
+
+let clear = false;
+const verify = setInterval(() => {
+	if (!clear) {
+		const positionObstacle = pipe.offsetLeft;
+		const jeffPosition = +window.getComputedStyle(jeff).bottom.replace("px", "");
+
+		if (positionObstacle <= 235 && jeffPosition < 100) {
+			pipe.style.animation = "none";
+			pipe.style.left = `${positionObstacle}px`;
+			gameboard.remove();
+			const cum = document.createElement("img");
+			cum.src = "./styles/images/jef-cum.png";
+			document.body.appendChild(cum);
+			clear = true;
+			setTimeout(() => {
+				if (confirm("Deseja Reiniciar?")) {
+					window.location.reload();
+				} else {
+					setTimeout(() => {
+						window.location.href = "https://www.instagram.com/ariel_bbgrl/";
+					}, 1000);
+				}
+			}, 1500);
+		}
+	}
+}, 10);
