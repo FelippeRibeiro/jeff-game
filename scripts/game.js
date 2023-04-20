7c3fda731c73f41e7ec820311050e2e3f6403b6d
FelippeRibeiro <felipper43@gmail.com>
2023-04-20 18:27:57 -0300

    New Images and animations time

--- scripts/game.js
+++ scripts/game.js
@@ -1,19 +1,30 @@
 const jeff = document.getElementById("jeff");
 const pipe = document.getElementById("pipe");
+const gameboard = document.getElementById("game-board");
+const penis = pipe.cloneNode(true);
 
+const assets = [
+	"./styles/images/pipe.png",
+	"./styles/images/penis.png",
+	"./styles/images/bullet.png",
+	"",
+];
 
-function jump(){
-    if(jeff.classList.contains("jump")) return
-    jeff.classList.add('jump')
+function jump() {
+	if (jeff.classList.contains("jump")) return;
+	jeff.classList.add("jump");
 
-    setTimeout(()=>{
-        jeff.classList.remove('jump');
-    },600)
+	setTimeout(() => {
+		jeff.classList.remove("jump");
+	}, 450);
 }
 
-document.addEventListener("keydown",(key)=>{
-    if(key.key === " ") jump()
-   
-})
+document.addEventListener("keydown", (key) => {
+	if (key.key === " ") jump();
+});
 
-document.addEventListener("click",jump)
+document.addEventListener("click", jump);
+
+setInterval(() => {
+	pipe.src = assets[Math.floor(Math.random() * 4)];
+}, 2400);
