bf9e06e7d87c5a649f87123462cd8d8ec39f5fda
FelippeRibeiro <104183971+FelippeRibeiro@users.noreply.github.com>
2023-04-20 14:10:19 -0300

    New background and styles
    Add: Background, Click to jump, Change: Obstacle, Upadate: Animations speed

--- scripts/game.js
+++ scripts/game.js
@@ -3,13 +3,17 @@ const pipe = document.getElementById("pipe");
 
 
 function jump(){
+    if(jeff.classList.contains("jump")) return
     jeff.classList.add('jump')
 
     setTimeout(()=>{
         jeff.classList.remove('jump');
     },600)
 }
+
 document.addEventListener("keydown",(key)=>{
     if(key.key === " ") jump()
    
 })
+
+document.addEventListener("click",jump)
