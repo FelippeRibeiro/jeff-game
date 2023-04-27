f2baee015e8572fcffd9462edfe2fde0ff66fd6e
FelippeRibeiro <felipper43@gmail.com>
2023-04-27 17:22:22 -0300

    .

--- scripts/game.js
+++ scripts/game.js
@@ -11,80 +11,80 @@ let pontos = 0;
 let fistTime = false;
 
 const assets = [
-  "./styles/images/pipe.png",
-  "./styles/images/penis.png",
-  "./styles/images/bullet.png",
+	"./styles/images/pipe.png",
+	"./styles/images/penis.png",
+	"./styles/images/bullet.png",
 ];
 
 function jump() {
-  if (jeff.classList.contains("jump")) return;
-  jeff.classList.add("jump");
+	if (jeff.classList.contains("jump")) return;
+	jeff.classList.add("jump");
 
-  setTimeout(() => {
-    jeff.classList.remove("jump");
-  }, 750);
+	setTimeout(() => {
+		jeff.classList.remove("jump");
+	}, 750);
 }
 
 document.addEventListener("keydown", (key) => {
-  if (key.key === " ") {
-    if (!fistTime) {
-      jeff.src = "./styles/images/jeff-bike2.jpeg";
-      fistTime = true;
-      pipe.classList.add("pipeAndAnimation");
-      pipe.style.display = "block";
-      InitAlert.remove();
-      return;
-    }
-    jump();
-  }
+	if (key.key === " ") {
+		if (!fistTime) {
+			jeff.src = "./styles/images/jeff-bike2.jpeg";
+			fistTime = true;
+			pipe.classList.add("pipeAndAnimation");
+			pipe.style.display = "block";
+			InitAlert.remove();
+			return;
+		}
+		jump();
+	}
 });
 
 document.addEventListener("click", () => {
-  if (!fistTime) {
-    fistTime = true;
-    pipe.classList.add("pipeAndAnimation");
-    pipe.style.display = "block";
-    return;
-  }
-  jump();
+	if (!fistTime) {
+		jeff.src = "./styles/images/jeff-bike2.jpeg";
+		fistTime = true;
+		pipe.classList.add("pipeAndAnimation");
+		pipe.style.display = "block";
+		InitAlert.remove();
+		return;
+	}
+	jump();
 });
 
 setInterval(() => {
-  pipe.src = assets[Math.floor(Math.random() * 3)];
+	pipe.src = assets[Math.floor(Math.random() * 3)];
 }, 3000);
 
 let clear = false;
 const verify = setInterval(() => {
-  if (!clear && pipe.classList.contains("pipeAndAnimation")) {
-    const positionObstacle = pipe.offsetLeft;
-    const jeffPosition = +window
-      .getComputedStyle(jeff)
-      .bottom.replace("px", "");
-    if (
-      positionObstacle <= 240 &&
-      positionObstacle > 5 &&
-      jeffPosition < 45 &&
-      !pipe.src.includes(assets[1].replace("./", ""))
-    ) {
-      pipe.style.animation = "none";
-      pipe.style.left = `${positionObstacle}px`;
-      gameboard.remove();
-      const cum = document.createElement("img");
-      cum.src = "./styles/images/jef-cum.png";
-      document.body.appendChild(cum);
-      audio.play();
-      clear = true;
-      setTimeout(() => {
-        if (confirm("Deseja Reiniciar?")) {
-          window.location.reload();
-        } else {
-          setTimeout(() => {
-            window.location.href = "https://www.instagram.com/ariel_bbgrl/";
-          }, 1000);
-        }
-      }, 1000);
-    }
-    contador.textContent = pontos;
-    pontos++;
-  }
+	if (!clear && pipe.classList.contains("pipeAndAnimation")) {
+		const positionObstacle = pipe.offsetLeft;
+		const jeffPosition = +window.getComputedStyle(jeff).bottom.replace("px", "");
+		if (
+			positionObstacle <= 240 &&
+			positionObstacle > 5 &&
+			jeffPosition < 45 &&
+			!pipe.src.includes(assets[1].replace("./", ""))
+		) {
+			pipe.style.animation = "none";
+			pipe.style.left = `${positionObstacle}px`;
+			gameboard.remove();
+			const cum = document.createElement("img");
+			cum.src = "./styles/images/jef-cum.png";
+			document.body.appendChild(cum);
+			audio.play();
+			clear = true;
+			setTimeout(() => {
+				if (confirm("Deseja Reiniciar?")) {
+					window.location.reload();
+				} else {
+					setTimeout(() => {
+						window.location.href = "https://www.instagram.com/ariel_bbgrl/";
+					}, 1000);
+				}
+			}, 1000);
+		}
+		contador.textContent = pontos;
+		pontos++;
+	}
 }, 10);
